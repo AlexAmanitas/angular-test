24425ffe722229aa7c3864da50b84c90abe5d154
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2023-01-23 20:52:25 +0200

    fin

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -8,30 +8,33 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
-  
 export class HeroDetailComponent {
-
   constructor(
-  private route: ActivatedRoute,
-  private heroService: HeroService,
-  private location: Location
-) {}
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   @Input() hero?: Hero;
-  
-  ngOnInit(): void {
-  this.getHero();
-}
 
-getHero(): void {
-  const id = Number(this.route.snapshot.paramMap.get('id'));
-  this.heroService.getHero(id)
-    .subscribe(hero => this.hero = hero);
-}
-  
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
+
   goBack(): void {
-  this.location.back();
-}
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
 }
